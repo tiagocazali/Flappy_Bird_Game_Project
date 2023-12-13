43fde913a894e57a92ef0bb9082ee91f0ee341f6
Tiago Cazali Magalhães <tiagocazali@gmail.com>
2023-12-13 19:17:10 +0100

    still trying to create the game

--- flappy_bird.py
+++ flappy_bird.py
@@ -1,52 +1,27 @@
+# Example file showing a basic pygame "game loop"
 import pygame
-import os
-import random
 
-TELA_LARGURA = 500
-TELA_ALTURA = 600
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((700, 500))
+clock = pygame.time.Clock()
+running = True
 
-IMAGEM_CANO_BASE = pygame.image.load(os.path.join('imgs', 'pipe_bottom.png'))
-IMAGEM_CANO_TOPO = pygame.image.load(os.path.join('imgs', 'pipe_top.png'))
-IMAGEM_CHAO = pygame.image.load(os.path.join('imgs', 'base.png'))
-IMAGEM_BACKGROUND = pygame.image.load(os.path.join('imgs', 'bg.png'))
-IMAGENS_PASSARO = [
-    pygame.image.load(os.path.join('imgs', 'bird1.png')),
-    pygame.image.load(os.path.join('imgs', 'bird2.png')),
-    pygame.image.load(os.path.join('imgs', 'bird3.png')),
-]
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
-pygame.font.init()
-FONTE_PONTOS = pygame.font.SysFont('arial', 50)
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("purple")
 
+    # RENDER YOUR GAME HERE
 
-class passaro:
-    IMGS = IMAGENS_PASSARO
-    ROTACAO_MAXIMA = 25
-    VELOCIDADE_ROTACAO = 26
-    TEMPO_ANIMACAO = 5
+    # flip() the display to put your work on screen
+    pygame.display.flip()
 
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.angulo = 0
-        self.velocidade = 0
-        self.altura = self.y
-        self.tempo = 0
-        self.contagem_imagem = 0
-        self.imagem = IMGS[0]
+    clock.tick(60)  # limits FPS to 60
 
-    def pular(self):
-        self.velocidade = -10.5
-        self.tempo = 0
-        self.altura = self.y
-
-    def mover(self):
-        self.tempo += 1
-        #S = S0 + V0T + AT²/2:
-        deslocamento = ????
-
-class cano:
-    pass
-
-class floor:
-    pass
+pygame.quit()
