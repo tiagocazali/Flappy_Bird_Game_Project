981b3e76701fdfb54f092c3595d51c1d18b31253
Tiago Cazali Magalhães <tiagocazali@gmail.com>
2023-12-15 03:25:08 +0100

    Pipe and bird Moving

--- flappy_bird.py
+++ flappy_bird.py
@@ -2,8 +2,6 @@ from typing import Any
 import pygame
 import random
 
-#from pygame.sprite import Group
-
 # pygame setup
 pygame.init()
 
@@ -19,8 +17,12 @@ ground = pygame.image.load("img/ground.png")
 
 #Game Variables
 ground_pos_x = 0
-ground_speed = 4
-game_active = True
+game_speed = 4
+game_active = False
+pipe_espace = 60
+pipe_frequency = 1500 #milliseconds
+last_pipe = pygame.time.get_ticks()
+
 
 
 class Bird(pygame.sprite.Sprite):
@@ -63,12 +65,37 @@ class Bird(pygame.sprite.Sprite):
         self.image = pygame.transform.rotate(self.image, self.gravity*-3) 
 
 
+class Pipe(pygame.sprite.Sprite):
+    def __init__(self, x, y, position):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = pygame.image.load("img/pipe.png")
+        self.rect = self.image.get_rect()
+
+        #If position is 1 = TOP Pipe, -1 is bottom pipe
+        if position == 1:
+            self.image = pygame.transform.flip(self.image, False, True)
+            self.rect.bottomleft = [x, y-pipe_espace]
+        if position == -1:    
+            self.rect.topleft = [x, y+pipe_espace]
+
+    def update(self):
+        #Move de pipe to Left
+        self.rect.x -= game_speed
+        
+        #remove pipe that is out of screen
+        if self.rect.right < 0:
+            self.kill()
+ 
+     
+
+pipe_group = pygame.sprite.Group()
 bird_group = pygame.sprite.Group()
 
 bird = Bird(100, int(screen_height/2))
-
 bird_group.add(bird)
-  
+
+ 
 
 running = True
 while running:
@@ -82,6 +109,7 @@ while running:
                 running = False
             if event.key == pygame.K_SPACE:
                 bird.gravity = -10 #Make the bird Jump
+                game_active = True #Stat the game moviments
 
     # Add de Background Image
     screen.blit(bg, (0,-150))
@@ -89,18 +117,40 @@ while running:
     #add the Bird
     bird_group.draw(screen)
     
+    #add pipes
+    pipe_group.draw(screen)
+    
     #add Floor
     screen.blit(ground, (ground_pos_x,(screen_height-70)))
+ 
 
     if game_active: 
 
         #make bird move
         bird_group.update()
-        if bird.rect.bottom > screen_height-75:
+        pipe_group.update() #make pipes move
+
+        if pygame.sprite.groupcollide(bird_group, pipe_group, False, False):
             game_active = False
 
+        #Create new pipes
+        time_now = pygame.time.get_ticks()
+        if time_now - last_pipe > pipe_frequency:
+            random_height = random.randint(-130, 130)
+            pipe_bottom = Pipe(screen_width, int(screen_height/2)+random_height, -1)
+            pipe_top = Pipe(screen_width, int(screen_height/2)+random_height, 1)
+            pipe_group.add(pipe_bottom)
+            pipe_group.add(pipe_top)
+            last_pipe = time_now
+       
+
+
+        #If bird hit the floor, Stop the Game
+        if bird.rect.bottom > screen_height-75:
+            game_active = False
+            
         #make the floor move
-        ground_pos_x -= ground_speed # move the floor a little to Letf - NEGATIVA VALUE
+        ground_pos_x -= game_speed # move the floor a little to Letf - NEGATIVA VALUE
         if abs(ground_pos_x) > 100:
             ground_pos_x = 0
 
